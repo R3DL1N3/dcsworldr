9b1e6f86698bfe99ddcd48d66014d6d9ab022827
R3DL1N3 <r0y@digitalcombatsimulator.world>
2022-07-04 06:42:31 +0100

    fix: normalisation ineffective

--- tests/testthat/test-utils-path.R
+++ tests/testthat/test-utils-path.R
@@ -1,6 +1,5 @@
 test_that("program files path works", {
   skip_on_os(c("mac", "linux", "solaris"))
   program_files.path() %>%
-    normalizePath() %>%
-    expect_equal(normalizePath("c:/program files"))
+    expect_equal("C:\\Program Files")
 })
